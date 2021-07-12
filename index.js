f294cb5875b3c9f05763559b8eb7547632ffbc47
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2021-07-12 12:57:21 +0000

    Added spaces and questions

--- index.js
+++ index.js
@@ -1,37 +1,47 @@
 var readlineSync = require('readline-sync');
 
-var score=0;
+var score = 0;
 
-var userName=readlineSync.question('What is your name? ');
+var userName = readlineSync.question('What is your name? ');
 
-console.log('Welcome '+userName+'to Do you know Avishkar?');
+console.log('Welcome ' + userName + ' to Do you know Avishkar?');
 
-function play(question,answer){
-  var userAns=readlineSync.question(question);
-  if(userAns===answer){
-    score=score+1;
+function play(question, answer) {
+  var userAns = readlineSync.question(question);
+  if (userAns === answer) {
+    score = score + 1;
     console.log('Right!');
-  }else{
-     console.log('Wrong!');
+  } else {
+    console.log('Wrong!');
   }
-  console.log('Current score is ',score);
+  console.log('Current score is ', score);
   console.log('________________________');
 }
 
-var questionOne={
-  question:'Where do I live? ',
-  answer:'Mumbai'
+var questionOne = {
+  question: 'Where do I live? ',
+  answer: 'Mumbai'
 };
 
-var questionTwo={
-  question:'My favorite superhero would be? ',
-  answer:'Batman'
+var questionTwo = {
+  question: 'My favorite superhero would be? ',
+  answer: 'Batman'
 };
 
-var questions=[questionOne,questionTwo];
+var questionThree = {
+  question: 'What did I do my bachelors in? ',
+  answer: 'Engineering'
+};
 
-for(let i=0;i<questions.length;i++){
-play(questions[i].question,questions[i].answer);
+var questionFour = {
+  question: 'What do I enoy doing? ',
+  answer: 'Working out'
+};
+
+var questions = [questionOne, questionTwo, questionThree, questionFour];
+
+for (let i = 0; i < questions.length; i++) {
+  play(questions[i].question, questions[i].answer);
 }
 
 
